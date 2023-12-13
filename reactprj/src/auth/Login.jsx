692122d0cbf1000545e6dd4ee78eb41d8372dc7a
Martí <msolert@fp.insjoaquimmir.cat>
2023-12-13 18:39:29 +0100

    2.3 finished

--- reactprj/src/auth/Login.jsx
+++ reactprj/src/auth/Login.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 
-export const Login = () => {
+const Login = () => {
   return (
-    <div>
-        <form action="/login" method="post">
-    <div>
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required/>
+    <div className="d-flex flex-column align-center border border-primary border-2 border-bottom-0 p-3 rounded-top container-md">
+      <form action="login">
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email:</label>
+          <input type="text" className="form-control" id="email" placeholder="example@example.com"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password:</label>
+          <input type="text" className="form-control" id="password"/>
+        </div>
+        <div className="mb-3">
+            <input className="btn btn-primary" type="submit" value="Login"/>
+        </div>
+      </form>
     </div>
-    <div>
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required/>
-    </div>
-    <div>
-      <input type="submit" value="Login"/>
-    </div>
-  </form>
-    </div>
+
   )
 }
+
+export default Login;
--- reactprj/src/auth/Register.jsx
+++ reactprj/src/auth/Register.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
-export const Register = () => {
+const Register = () => {
   return (
-    <div>Register</div>
+    <div className="d-flex flex-column align-center border border-2 border-top-1 border-primary p-3 rounded-bottom container-md">
+      <form action="register">
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email:</label>
+          <input type="text" className="form-control" id="email" placeholder="example@example.com"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password:</label>
+          <input type="text" className="form-control" id="password"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password_confirm" className="form-label">Confirm password:</label>
+          <input type="text" className="form-control" id="password_confirm"/>
+        </div>
+        <div className="mb-3">
+            <input className="btn btn-primary" type="submit" value="Register"/>
+        </div>
+      </form>
+    </div>
+
   )
 }
+
+export default Register;
